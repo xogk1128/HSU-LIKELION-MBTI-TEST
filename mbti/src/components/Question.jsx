0aafd43ab6ee8981c54621590fb9cd1002c39b18
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-07-05 03:58:17 +0900

    ✨ 과제 수정
    과제 수정

--- mbti/src/components/Question.jsx
+++ mbti/src/components/Question.jsx
@@ -1,104 +1,104 @@
 import React from "react";
 
-import qs1 from "../image/qs1.png";
+import qs1 from "../image/qs1.jpeg";
 import qs2 from "../image/qs2.png";
-import qs3 from "../image/qs3.png";
-import qs4 from "../image/qs4.png";
+import qs3 from "../image/qs3.jpeg";
+import qs4 from "../image/qs4.jpeg";
 import qs5 from "../image/qs5.png";
 import qs6 from "../image/qs6.png";
-import qs7 from "../image/qs7.png";
-import qs8 from "../image/qs8.png";
+import qs7 from "../image/qs7.jpeg";
+import qs8 from "../image/qs8.jpeg";
 import qs9 from "../image/qs9.png";
 import qs10 from "../image/qs10.png";
 import qs11 from "../image/qs11.png";
-import qs12 from "../image/qs12.png";
+import qs12 from "../image/qs12.jpg";
 import "../App.css";
 import { useNavigate } from "react-router-dom";
 
 const questions = {
     1: {
-        title: "아무일도 없는 나른한 오후, 친구가 놀자며 카톡을 보냈다.",
+        title: "친구랑 놀고 집에 갈 때 나는?",
         image: qs1,
         type: "EI",
-        A: "빠르게 준비를 마치고 친구를 만나러 나간다.",
-        B: "얼마만의 휴일인데... 집에서 휴식을 취한다.",
+        A: "아 잘놀았다! 완전 충전됐다!!",
+        B: "아 잘 놀았다! 이제 집가서 쉬면서 충전해야지..",
     },
     2: {
-        title: "소개팅을 갔는데 분위기가 너무 어색하다.",
+        title: "친구를 생일파티에 초대하려고 한다. 나는?",
         image: qs2,
         type: "EI",
-        A: "어색함을 견디지 못하고 말을 먼저 꺼낸다.",
-        B: "너무 어색해 미치겠지만 가만히 있는다.",
+        A: "어느정도 친한 친구도 초대한다.",
+        B: "찐친 몇명만 초대한다.",
     },
     3: {
-        title: "자취를 하려고 집을 구하러 다닌다.",
+        title: "사과를 보면 떠오르는 생각은? 또는 사과 하면 떠오르는 것은?",
         image: qs3,
         type: "SN",
-        A: "방 구조, 수압, 방음 등을 꼼꼼하게 체크한다.",
-        B: "방이 화사한지, 넓은지, 포근한지 등을 본다.",
+        A: "빨갛다, 맛있다, 사각사각, 새콤달콤, 둥글다.",
+        B: "백설공주, 비타민C, 뉴턴, 스티브잡스, 아이폰",
     },
     4: {
-        title: "바로 당장 내일이 시험인데 공부하기가 너무 싫다.",
+        title: "비행기를 탔는데 갑자기 흔들린다면 드는 생각은?",
         image: qs4,
         type: "SN",
-        A: "범위가 줄었으면 좋겠다. 이번 시험 잘 봐야 하는데..",
-        B: "시험은 왜 봐야 하는거지..? 시험이 사라지는 시대도 올까?",
+        A: "어라 비행기 흔들리네 멀미나겠다..",
+        B: "낙하산 타고 비행기 탈출하는 모습까지 그리기 시작. 비행기 부품이 망가지고 있는 영화 장면 오버랩 됨.",
     },
     5: {
-        title: "화났을 때 나는?",
+        title: "친구가 여자/남자친구와 헤어졌다고 한다. 이때 나의 반응은?",
         image: qs5,
         type: "TF",
-        A: "논리적으로 잘 말하면서 따진다.",
-        B: "할말이 많지만 너무 분해서 눈물부터 난다.",
+        A: "사람은 사람으로 잊는거래 힘내",
+        B: "괜찮아?? 많이 힘들겠다.. 내가 전화할게!!",
     },
     6: {
-        title: "고민을 얘기하는 친구, 듣다보니 친구의 잘못인 것 같다.",
+        title: "친구가 택배를 뜯다가 칼에 손이 베였다고 한다. 이때 나의 반응은?",
         image: qs6,
         type: "TF",
-        A: "친구의 잘못된 점을 말해준다.",
-        B: "직접적으로 말하면 친구가 상처받을까봐 돌려 말한다.",
+        A: "얼마나 다쳤는데?",
+        B: "괜찮아?? 많아 아팠겠다... 병원은 가봤어?",
     },
     7: {
-        title: "준비물을 챙겨야할 때 나는?",
+        title: "여행 가기 전 나는?",
         image: qs7,
         type: "PJ",
         A: "굳이 하루전에...? 당일날 챙기면 충분하다!",
         B: "며칠 전에 미리 챙겨 준비해둔다!",
     },
     8: {
-        title: "여행 계획을 짤 때 나는?",
+        title: "과제를 해야한다.. 나는?",
         image: qs8,
         type: "PJ",
-        A: "그때 그때 가고 싶은 곳 가고 먹고 싶은 것 먹는 것이 낭만이지!",
-        B: "버스 시간, 가격, 메뉴까지 미리 다 정해서 계획을 짜둔다.",
+        A: "오늘 제출일이니까 슬슬 해볼까?",
+        B: "다른 과제도 있으니까 미리 해야겠다!!",
     },
     9: {
-        title: "곧 워터밤 페스티벌이 열린다고 한다.",
+        title: "친구랑 놀던 도중 친구의 친구도 같이 놀아도 되냐고 질문한다. 나는?",
         image: qs9,
         type: "EI",
-        A: "아껴뒀던 수영복과 물총을 꺼내 페스티벌을 즐기러 간다.",
-        B: "집에서도 충분히 즐길 수 있어! 유튜브로 시청한다.",
+        A: "너무 좋지! 오늘부터 너도 내 친구다.",
+        B: "아 그래..? (제발 제발 안돼)",
     },
     10: {
-        title: "비행기를 탔는데 갑자기 흔들린다.",
+        title: "아무 생각도 하지말라고 했을 때 나는?",
         image: qs10,
         type: "SN",
-        A: "최대한 침착하며 안전벨트를 꽉 매고 주위를 살핀다.",
-        B: "비행기가 갑자기 추락하면 어쩌지?? 혹시 누가 테러를 일으킨 건 아닐까??",
+        A: "진짜 아무 생각도 안함.",
+        B: "온갖 생각들 때문에 아무 생각도 안할 수 없음.",
     },
     11: {
-        title: "친구가 갑자기 차 사고가 났다고 한다.",
+        title: "친구가 갑자기 힘들어보인다고 키프티콘을 준다. 이때 나의 반응은?",
         image: qs11,
         type: "TF",
-        A: "헐 보험은 들어놨지?? 보험사에서는 뭐래??",
-        B: "헐 몸은 괜찮아?? 다친데는 없고...?",
+        A: "고마워 잘 먹을게! (내가 힘들어 보였나?)",
+        B: "헐 감동이야.. ㅠㅠ 잘 먹을게!!! (진짜 감동)",
     },
     12: {
-        title: "오랜만에 밖에서 외식을 하려고 한다.",
+        title: "갑자기 친구에게 연락이 와 나오라고 한다. 나는?",
         image: qs12,
         type: "PJ",
-        A: "같은 곳은 지겨워! 새로운 곳을 탐방한다!",
-        B: "매번 가던 곳으로! 편안하고 맛있어서 좋다.",
+        A: "(나가는 중이라 답장 안함)",
+        B: "아 갑자기는 안되고 다음에 보자 ㅠㅠㅜ",
     },
 };
 
@@ -120,7 +120,7 @@ const Question = ({ num, setNum, updateScore }) => {
             <p id="pgrNum">{num}/12</p>
             <div className="progress">
                 <div
-                    className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
                     style={{ width: `calc(100/12*${num}%)` }}
                 ></div>
             </div>
@@ -134,13 +134,13 @@ const Question = ({ num, setNum, updateScore }) => {
                 )}
             </div>
             <button
-                className="btn btn-warning"
+                className="btn btn-success"
                 onClick={() => handleAnswer(currentQuestion.type, 1)}
             >
                 {currentQuestion.A}
             </button>
             <button
-                className="btn btn-warning mt-3"
+                className="btn btn-success mt-3"
                 onClick={() => handleAnswer(currentQuestion.type, 0)}
             >
                 {currentQuestion.B}
--- mbti/src/components/Result.jsx
+++ mbti/src/components/Result.jsx
@@ -1,115 +1,115 @@
 import React from "react";
-import isfjImg from "../image/원장 선생님.png";
-import isfpImg from "../image/훈이.png";
-import istjImg from "../image/채성아.png";
-import istpImg from "../image/옆집 아줌마.png";
-import infjImg from "../image/이슬이누나.png";
-import infpImg from "../image/짱아.png";
-import intjImg from "../image/오수형.png";
-import intpImg from "../image/맹구.png";
-import esfjImg from "../image/신형만.png";
-import esfpImg from "../image/봉미선.png";
-import estjImg from "../image/철수.png";
-import estpImg from "../image/수지.png";
-import enfjImg from "../image/유리.png";
-import enfpImg from "../image/흰둥이.png";
-import entjImg from "../image/나미리선생님.png";
-import entpImg from "../image/짱구.png";
+import isfjImg from "../image/isfj.png";
+import isfpImg from "../image/isfp.png";
+import istjImg from "../image/istj.png";
+import istpImg from "../image/istp.png";
+import infjImg from "../image/infj.png";
+import infpImg from "../image/infp.png";
+import intjImg from "../image/intj.png";
+import intpImg from "../image/intp.png";
+import esfjImg from "../image/esfj.png";
+import esfpImg from "../image/esfp.png";
+import estjImg from "../image/estj.png";
+import estpImg from "../image/estp.png";
+import enfjImg from "../image/enfj.png";
+import enfpImg from "../image/enfp.png";
+import entjImg from "../image/entj.png";
+import entpImg from "../image/entp.png";
 import { useNavigate } from "react-router-dom";
 
 const results = {
     ISFJ: {
-        mbti: "원장 선생님",
+        mbti: "ISFJ",
         explain:
             "따뜻하고 친근한 성격이며, 책임감이 있고 인내력이 강해요! 그룹에서 리더보다는 서포터의 역할을 선호하며, 소외되는 팀원이 있으면 잘 이끌어서 도움을 줘요. 남의 부탁을 잘 거절 못하고 잘 들어주는 편이에요.",
         img: isfjImg,
     },
     ISFP: {
-        mbti: "훈이",
+        mbti: "ISFP",
         explain:
             "사람을 만나는 건 좋지만 싫기도하고 그러다가도 만나면 잘 노는 타입이에요! 다른 사람의 부탁을 잘 거절하지 못하고 양보를 잘하며, 타인에게 민폐 끼치는 걸 싫어하는 성격이에요. 발등에 불 떨어질 때까지 일을 미루기도 해요.",
         img: isfpImg,
     },
     ISTJ: {
-        mbti: "채성아 선생님",
+        mbti: "ISTJ",
         explain:
             "신중하고 조용한 편이며 집중력이 강하고 매사에 철저한 성격이에요! 책임감이 강하고 성취해야 한다고 생각하는 일에는 꾸준하게 추진해 나가는 유형이며, 타인에게 속마음을 잘 드러내지 않는 편이에요.",
         img: istjImg,
     },
     ISTP: {
-        mbti: "옆집 아주머니",
+        mbti: "ISTP",
         explain:
             "타인을 잘 도우며 다른 사람들과 경험을 공유하는 것을 좋아하는 성격이에요! 직접 손으로 만지고 눈으로 보면서 세상을 탐색하는 것을 좋아하며 무언가를 만드는 데 타고난 재능을 가졌어요.",
         img: istpImg,
     },
     INFJ: {
-        mbti: "이슬이누나",
+        mbti: "INFJ",
         explain:
             "인내심이 많고 통찰력과 직관력이 뛰어나며 화합을 추구하는 성격이에요! 친해지고 나면 첫인상과 다른 경우가 있으며, 몽환적이고 신비로운 것을 좋아해요. 분쟁이나 다툼을 싫어하는 편이에요.",
         img: infjImg,
     },
     INFP: {
-        mbti: "짱아",
+        mbti: "INFP",
         explain:
             "사람 만나는걸 좋아하면서도 싫어하는, 집 들어가면 연락 두절되는 조용한 관종이에요! 어디로 튈지 모르는 탱탱볼 같은 매력이 있어요. 인간 관계에선 눈치가 빨라 섬세한 부분까지 잘 캐치하는 성격이에요.",
         img: infpImg,
     },
     INTJ: {
-        mbti: "오수",
+        mbti: "INTJ",
         explain:
             "본인이 수립한 목표를 향해 끊임없이 노력하고, 세상의 많은 것들에 지적 호기심을 갖는 성격이에요! 리더로서 자신의 행동이나 결과에 철저하게 책임지는 책임감을 가지고 있어요. 쉽고 간단한 일에도 항상 최선을 다하는 성격이에요.",
         img: intjImg,
     },
     INTP: {
-        mbti: "맹구",
+        mbti: "INTP",
         explain:
             "분석적이고 논리적이며 객관적인 비평을 잘 하는 성격이에요! 이해가 빠르고 직관력, 통찰력이 있으며 때로는 조용하고 과묵하기도 해요. 자기가 원하는 것이 아니면 별로 관심을 갖지는 않아요.",
         img: intpImg,
     },
     ESFJ: {
-        mbti: "신형만",
+        mbti: "ESFJ",
         explain:
             "눈치가 빠르고 분위기를 잘 맞춰주는 성격이에요! 맡은 일을 정확한 시간에 맞추어 끝내는 것을 중요시하고, 주변 사람들이 무엇이 필요한지 쉽게 찾아 도움을 줘요. 조화를 추구하기 때문에 갈등은 싫어해요.",
         img: esfjImg,
     },
     ESFP: {
-        mbti: "봉미선",
+        mbti: "ESFP",
         explain:
             "어디에서나 분위기 메이커를 담당하며 사교적이고 활동적인 성격이에요! 어떤 상황에서든 잘 적응하는 편이며, 주변 사람들에게 관심이 많고 그들의 일에 진정으로 공감해주는 성격이에요.",
         img: esfpImg,
     },
     ESTJ: {
-        mbti: "철수",
+        mbti: "ESTJ",
         explain:
             "강한 의지력과 끈기를 지닌 성격이에요! 모든 일에 책임감을 가지고 있으며 목표한 바를 이루기 위해 노력해요. 매 일에 도전적이고 호기심을 가지고 있어요. 외로움을 잘 타지 않는 성격이에요.",
         img: estjImg,
     },
     ESTP: {
-        mbti: "수지",
+        mbti: "ESTP",
         explain:
             "항상 모든 일에 자신감이 넘치는 성격이에요! 모임에 가면 분위기 메이커 역할을 하며 스릴을 즐기는 성격이에요! 오늘의 일은 내일로 미루는 것을 좋아하며 오늘만 산다라는 생각으로 살아가기도 해요.",
         img: estpImg,
     },
     ENFJ: {
-        mbti: "유리",
+        mbti: "ENFJ",
         explain:
             "사람을 좋아하며 항상 타인에게 잘 맞춰주는 성격이에요! 소소한 거에도 배려심이 많으며 진실성을 중요시하고, 매사에 책임감이 강한 타입이에요. 따라서 많은 사람들에게 신뢰를 받는 성격이에요.",
         img: enfjImg,
     },
     ENFP: {
-        mbti: "흰둥이",
+        mbti: "ENFP",
         explain:
             "긍정적이고 낙천적인 성격을 가지고 있어요! 공감능력이 좋아서 친구들의 고민 상담을 잘 해줘요! 즉흥적인 행동을 좋아하는 편이며 지루하고 재미없는 일상에 질려하는 편이에요.",
         img: enfpImg,
     },
     ENTJ: {
-        mbti: "나미리 선생님",
+        mbti: "ENTJ",
         explain:
             "미래를 향한 뚜렷한 목표를 가지고 빈틈없이 행동으로 옮기는 성격이에요! 집에 있으면 무기력해져서 무엇이라도 해야 하는 편이며 친구들이 고민 상담하면 해결책을 찾아주는 걸 좋아해요.",
         img: entjImg,
     },
     ENTP: {
-        mbti: "짱구",
+        mbti: "ENTP",
         explain:
             "사교적인 성격에 타인의 생각을 읽고 이해하는 능력이 뛰어난 타입이에요! 새로운 아이디어나 창의적인 발상을 잘 하며, 두뇌회전이 빠르고 이해심이 뛰어나요. 지속적으로 새로운 것들에 도전하는 것을 좋아해요.",
         img: entpImg,
@@ -144,17 +144,17 @@ const Result = ({ score, resetState }) => {
     return (
         <div className="content" id="result">
             <img id="img" className="rouded-circle" src={result.img}></img>
-            <p id="qu">나와 닮은 짱구 캐릭터는?</p>
+            <p id="qu">나의 MBTI는?</p>
             <p id="mbti">{result.mbti}</p>
-            <p id="explain" className="card card-body border-warning">
+            <p id="explain" className="card card-body border-success">
                 {result.explain}
             </p>
             <div className="result-btn">
-                <button className="btn btn-warning" onClick={handleRestart}>
+                <button className="btn btn-success" onClick={handleRestart}>
                     다시하기
                 </button>
                 <button
-                    className="btn btn-warning mt-4"
+                    className="btn btn-success mt-4"
                     onClick={handleCopyLink}
                 >
                     링크복사
